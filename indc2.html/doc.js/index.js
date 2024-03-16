30af411e23ddf17e557108dcbd64ddd8fedfeb43
Elideangelo <124262799+Elideangelo@users.noreply.github.com>
2024-03-16 16:10:15 -0300

    https://github.com/Elideangelo/Documentos.git

---
+++ indc2.html/doc.js/index.js
@@ -0,0 +1,15 @@
+import{ shapes } from 
+grasshopper.shapes
+function setLastValuue(values,newLastValues){
+    
+let lastIndex=values.length-1;
+values[lastIndex]=newLastValues;
+function extrakeys() {
+ lastIndex-=1+1;
+ lastIndex+=1*1;  
+}
+}
+sertLastvalues(shapes,'hexagon')
+for (let item of shapes) {
+ console.log(item) ;  
+}
